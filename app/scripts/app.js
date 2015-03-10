388df1fde2900eb48884becfa8d8553811bc20b2
Antoine Pultier <antoine.pultier@sintef.no>
2015-03-10 14:45:15 +0100

    upload route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/upload', {
+        templateUrl: 'views/upload.html',
+        controller: 'UploadCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/upload.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grafterizerApp.controller:UploadCtrl
+ * @description
+ * # UploadCtrl
+ * Controller of the grafterizerApp
+ */
+angular.module('grafterizerApp')
+  .controller('UploadCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
