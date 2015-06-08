8462b7b4722601306987b1a57f249fde500f6c1f
Antoine Pultier <antoine.pultier@sintef.no>
2015-06-08 13:09:23 +0200

    dataset upload

--- app/scripts/controllers/dataset.js
+++ app/scripts/controllers/dataset.js
@@ -32,4 +32,32 @@ angular.module('grafterizerApp')
   	$scope.selectDistribution = function(distribution) {
   		$state.go('distribution', {id: distribution});
   	};
+
+  	$scope.$watch('file', function() {
+  		if ($scope.file && $scope.file[0]) {
+  			var file = $scope.file[0];
+  			console.log(file);
+
+  			var metadata = {
+	            '@context': ontotextAPI.getContextDeclaration(),
+	            '@type': 'dcat:Distribution',
+	            'dct:title': "undefined title",
+	            'dct:description': "undefined description",
+	            'dcat:fileName': file.name,
+	            'dcat:mediaType': file.type
+	            // 'dct:public': $scope.document['dct:public'],
+	            // 'dct:modified': moment().format("YYYY-MM-DD")
+	        };
+	  		ontotextAPI.uploadDistribution(id,
+	  			file, metadata).success(function(){
+	  			$state.transitionTo($state.current, $stateParams, {
+				    reload: true,
+				    inherit: false,
+				    notify: true
+				});
+	  		});
+
+	  		// $state.go
+  		}
+  	});
   });
