1133c3bb24408d8253c5ac3e7f26fa50314d317e
Nikolay Nikolov <nikolay.nikolov@sintef.no>
2015-06-11 17:08:55 +0200

    Fixed transformation revival

--- app/scripts/controllers/transformation.js
+++ app/scripts/controllers/transformation.js
@@ -8,103 +8,107 @@
  * Controller of the grafterizerApp
  */
 angular.module('grafterizerApp')
-  .controller('TransformationCtrl', function (
-    $scope,
-    $stateParams,
-    ontotextAPI,
-    $rootScope,
-    $state,
-    $mdToast,
-    $mdDialog,
-    transformationDataModel) {
+    .controller('TransformationCtrl', function (
+                $scope,
+                 $stateParams,
+                 ontotextAPI,
+                 $rootScope,
+                 $state,
+                 $mdToast,
+                 $mdDialog,
+                 transformationDataModel) {
 
-  	var id = $scope.id = $stateParams.id;
+    var id = $scope.id = $stateParams.id;
     $scope.document = {
-      title: 'loading'
+        title: 'loading'
     };
 
     ontotextAPI.transformation(id).success(function(data){
-      $scope.document = data;
-      $scope.document.title = data['dct:title'];
-      $scope.document.description = data['dct:description'];
+        $scope.document = data;
+        $scope.document.title = data['dct:title'];
+        $scope.document.description = data['dct:description'];
     }).error(function(){
-      $state.go('^');
+        $state.go('^');
     });
 
     ontotextAPI.getClojure(id).success(function(data){
-      console.log(data);
-      $scope.clojure = data;
+        console.log(data);
+        $scope.clojure = data;
     });
 
     ontotextAPI.getJson(id).success(function(data){
-      var transformation = transformationDataModel.
-        Transformation.revive(data);
-      $scope.transformation = transformation; 
-      if (transformation.pipelines && transformation.pipelines.length) {
-        $scope.pipeline = transformation.pipelines[0]; 
-      } else {
-        $scope.pipeline = new transformationDataModel.Pipeline([]);
-        transformation.pipelines = [$scope.pipeline];
-      }
+        var transformation = JSON.parse(data, function (key, value) {
+            return key === '' && value.hasOwnProperty('__type')
+                ? transformationDataModel[value.__type].revive(value) : this[key];
+        });
+
+        console.log(transformation);
+        $scope.transformation = transformation; 
+        if (transformation.pipelines && transformation.pipelines.length) {
+            $scope.pipeline = transformation.pipelines[0]; 
+        } else {
+            $scope.pipeline = new transformationDataModel.Pipeline([]);
+            transformation.pipelines = [$scope.pipeline];
+        }
     });
 
     $rootScope.actions = {
-      save: function(){
-        var update = angular.copy($scope.document);
-        update['dct:title'] = update.title;
-        update['dct:description'] = update.description;
-        update['dct:modified'] = moment().format("YYYY-MM-DD");
-        delete update.title;
-        delete update.description;
-        // delete update['dct:clojureDataID'];
-        // delete update['dct:jsonDataID'];
-        delete update['dct:publisher'];
+        save: function(){
+            var update = angular.copy($scope.document);
+            update['dct:title'] = update.title;
+            update['dct:description'] = update.description;
+            update['dct:modified'] = moment().format("YYYY-MM-DD");
+            delete update.title;
+            delete update.description;
+            // delete update['dct:clojureDataID'];
+            // delete update['dct:jsonDataID'];
+            delete update['dct:publisher'];
 
-        ontotextAPI.updateTransformation(update,
-          $scope.clojure+"-*", $scope.transformation);
-      },
-      delete: function(ev) {
-        var confirm = $mdDialog.confirm()
-          .title('Do you really want to delete this transformation?')
-          .content('It\'s a nice transformation')
-          .ariaLabel('Deletion confirmation')
-          .ok('Please do it!')
-          .cancel('I changed my mind, I like it')
-          .targetEvent(ev);
+            ontotextAPI.updateTransformation(update,
+                                             $scope.clojure+"-*", $scope.transformation);
+        },
+        delete: function(ev) {
+            var confirm = $mdDialog.confirm()
+            .title('Do you really want to delete this transformation?')
+            .content('It\'s a nice transformation')
+            .ariaLabel('Deletion confirmation')
+            .ok('Please do it!')
+            .cancel('I changed my mind, I like it')
+            .targetEvent(ev);
 
-        $mdDialog.show(confirm).then(function() {
-          ontotextAPI.deleteTransformation(id).success(function(){
-            $state.go('transformations');
-            $mdToast.show(
-              $mdToast.simple()
-                .content('Transformation "'+$scope.document.title+'" deleted')
-                .position('bottom left')
-                .hideDelay(6000)
-            );
-          });
-        });
-      },
-      fork: function(ev) {
-        var transformationJSON = JSON.stringify($scope.transformation);
-        ontotextAPI.newTransformation({
-            '@context': ontotextAPI.getContextDeclaration(),
-            '@type': 'dcat:Transformation',
-            'dct:title': $scope.document.title+"-fork",
-            'dct:description': $scope.document.description,
-            'dct:public': $scope.document['dct:public'],
-            'dct:modified': moment().format("YYYY-MM-DD")
-          }, "this is clojure", transformationJSON)
-          .success(function(data){
-            $mdToast.show(
-              $mdToast.simple()
-                .content('Transformation forked')
-                .position('bottom left')
-                .hideDelay(6000)
-              );
-            $state.go('transformations.transformation', {
-              id: data['@id']
+            $mdDialog.show(confirm).then(function() {
+                ontotextAPI.deleteTransformation(id).success(function(){
+                    $state.go('transformations');
+                    $mdToast.show(
+                        $mdToast.simple()
+                        .content('Transformation "'+$scope.document.title+'" deleted')
+                        .position('bottom left')
+                        .hideDelay(6000)
+                    );
+                });
             });
-          });
-      }
+        },
+        fork: function(ev) {
+            var transformationJSON = JSON.stringify($scope.transformation);
+            ontotextAPI.newTransformation({
+                '@context': ontotextAPI.getContextDeclaration(),
+                '@type': 'dcat:Transformation',
+                'dct:title': $scope.document.title+"-fork",
+                'dct:description': $scope.document.description,
+                'dct:public': $scope.document['dct:public'],
+                'dct:modified': moment().format("YYYY-MM-DD")
+            }, "this is clojure", transformationJSON)
+                .success(function(data){
+                $mdToast.show(
+                    $mdToast.simple()
+                    .content('Transformation forked')
+                    .position('bottom left')
+                    .hideDelay(6000)
+                );
+                $state.go('transformations.transformation', {
+                    id: data['@id']
+                });
+            });
+        }
     };
-  });
+});
