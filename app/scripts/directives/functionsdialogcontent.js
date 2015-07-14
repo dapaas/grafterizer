d9539c52d047ebf3dc440d222eb692499252e3e8
Antoine Pultier <antoine.pultier@sintef.no>
2015-07-14 15:23:52 +0200

    Few improvements (again)

--- app/scripts/directives/functionsdialogcontent.js
+++ app/scripts/directives/functionsdialogcontent.js
@@ -13,7 +13,6 @@ angular.module('grafterizerApp')
       restrict: 'E',
       scope: {
         function: '=',
-       // selectedFunctionName: '=',
         transformation: '='
       },
       link: function postLink(scope, element, attrs) {
@@ -22,31 +21,61 @@ angular.module('grafterizerApp')
         };
 
         scope.availableFunctions = [
-        {name: "columns", selected: false},    
-        {name: "derive-column", selected: false},
-        {name: "drop-rows", selected: false},
-        {name: "make-dataset", selected: false},    
-        {name: "mapc", selected: false},    
-        {name: "take-rows", selected: false},    
-        {name: "custom-code", selected: false}    
+          {
+            name: 'columns',
+            selected: false
+          },
+          {
+            name: 'derive-column',
+            selected: false
+          },
+          {
+            name: 'drop-rows',
+            selected: false
+          },
+          {
+            name: 'make-dataset',
+            selected: false
+          },
+          {
+            name: 'mapc',
+            selected: false
+          },
+          {
+            name: 'take-rows',
+            selected: false
+          },
+          {
+            name: 'custom-code',
+            selected: false
+          }
         ];
         scope.selectedFunctionName = null;
         scope.changeSelected = function(f) {
-        var i;
-        for (i=0;i<scope.availableFunctions.length; ++i) {
-            if (scope.availableFunctions[i].name === f) {scope.availableFunctions[i].selected=true;
-            
+          var i;
+          for (i = 0; i < scope.availableFunctions.length; ++i) {
+            if (scope.availableFunctions[i].name === f) {
+              scope.availableFunctions[i].selected = true;
+
+            } else {
+              scope.availableFunctions[i].selected = false;
             }
-            else {scope.availableFunctions[i].selected=false;}
-        }
-        scope.selectedFunctionName=f;
-        }
+          }
+
+          scope.selectedFunctionName = f;
+        };
+
         scope.add = function() {
           $mdDialog.hide(scope.generateCurrFunction());
         };
+
         scope.closeDialog = function() {
           $mdDialog.cancel();
         };
+
+        if (scope.function) {
+          scope.changeSelected(scope.function.name);
+        }
       }
     };
   });
