d6823e338db9eaeb5be450da14b6279b4498682a
Haavard Heitlo Holm <havard.heitlo.holm@sintef.no>
2015-09-28 15:23:17 +0200

    updated template code

--- app/scripts/services/jarfterservice.js
+++ app/scripts/services/jarfterservice.js
@@ -32,7 +32,7 @@ angular.module('grafterizerApp')
     api.generateClojure = function(transformation) {
       
       // namespace declaration and imports
-      var clojure = "(ns sintef-jarfter-template.core\r\n  (:require\r\n   [grafter.rdf :refer [s add prefixer]]\r\n   [grafter.tabular :refer :all]\r\n   [grafter.rdf.io :as ses]\r\n   [grafter.rdf.templater :refer [graph]]\r\n   [grafter.tabular.common :refer [read-dataset*]]\r\n   [grafter.vocabularies.rdf :refer :all]\r\n   [grafter.vocabularies.foaf :refer :all]\r\n   [grafter.rdf.formats :refer :all]\r\n   [clojure.string :refer [trim trim-newline triml trimr upper-case lower-case capitalize]]\r\n   )\r\n   (:gen-class)\r\n)";
+      var clojure = "\r\n(ns grafterizer.transformation\r\n  (:require\r\n   [grafter.rdf :refer [s add prefixer]]\r\n   [grafter.tabular :refer :all]\r\n   [grafter.rdf.io :as ses]\r\n   [grafter.rdf.templater :refer [graph]]\r\n   [grafter.tabular.common :refer [read-dataset*]]\r\n   [grafter.vocabularies.rdf :refer :all]\r\n   [grafter.vocabularies.foaf :refer :all]\r\n   [grafter.rdf.formats :refer :all]\r\n   [clojure.string :refer [trim]]\r\n   )\r\n)";
       
       // pipeline and graft
       clojure += generateClojure.fromTransformation(transformation);
@@ -41,10 +41,10 @@ angular.module('grafterizerApp')
       if (rScope.transformation.graphs &&
           rScope.transformation.graphs.length !== 0) {
         // if graft - execute the pipeline and then the graft
-        clojure += "(defn import-data\r\n  [quads-seq destination]\r\n  (add (ses\/rdf-serializer destination) quads-seq)\r\n)\r\n\r\n(defn -main [& [path output]]\r\n  (when-not (and path output)\r\n    (println \"Usage: lein run <input-file.csv> <output-file.(nt|rdf|n3|ttl)>\")\r\n    (System\/exit 0))\r\n\r\n  \r\n  (import-data \r\n    (make-graph (my-pipe (read-dataset path)))\r\n  output)\r\n\r\n  (println path \"=>\" output)\r\n)";
+        clojure += "(defn import-data\r\n  [quads-seq destination]\r\n  (add (ses\/rdf-serializer destination) quads-seq)\r\n)\r\n\r\n(defn my-transformation [dataset output]\r\n\r\n  (import-data \r\n    (make-graph (my-pipe dataset))\r\n  output)\r\n";
       } else {
         // if pipe - execute just pipe
-        clojure += "(defn import-data\r\n  [pipe-result destination]\r\n  (write-dataset destination pipe-result)\r\n)\r\n\r\n(defn -main [& [path output]]\r\n  (when-not (and path output)\r\n    (println \"Usage: lein run <input-file.csv> <output-file.csv>\")\r\n    (System\/exit 0))\r\n\r\n  \r\n  (import-data \r\n    (my-pipe (read-dataset path))\r\n  output)\r\n\r\n  (println path \"=>\" output)\r\n)";
+        clojure += "(defn import-data\r\n  [pipe-result destination]\r\n  (write-dataset destination pipe-result)\r\n)\r\n\r\n(defn my-transformation [dataset output]\r\n\r\n  (import-data \r\n    (make-graph (my-pipe dataset))\r\n  output)\r\n";
       }
 
       console.log(clojure);
