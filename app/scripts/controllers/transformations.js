5a88fafbe45cc24d6ff0deb678593dedad0604a0
Antoine Pultier <antoine.pultier@sintef.no>
2015-10-01 14:13:22 +0200

    It is now possible to search and browse public transformations

--- app/scripts/controllers/transformations.js
+++ app/scripts/controllers/transformations.js
@@ -3,13 +3,27 @@
 angular.module('grafterizerApp')
   .controller('TransformationsCtrl', function($scope, ontotextAPI, $state) {
 
-    ontotextAPI.publicTransformations().success(function(data) {
-      $scope.transformations = data['dcat:record'].reverse();
-    });
-
     $scope.selectTransformation = function(transformation) {
       $state.go('transformations.transformation', {
         id: transformation['foaf:primaryTopic']
       });
     };
+
+    var showTransformations = function(data) {
+      $scope.transformations = data['dcat:record'].reverse();
+    };
+
+    $scope.load = function() {
+      $scope.transformations = [];
+      if ($scope.searchinput) {
+        ontotextAPI.searchTransformations($scope.searchinput, $scope.showPublic ? 'y' : 'n')
+         .success(showTransformations);
+      } else if ($scope.showPublic) {
+        ontotextAPI.publicTransformations().success(showTransformations);
+      } else {
+        ontotextAPI.transformations().success(showTransformations);
+      }
+    };
+
+    $scope.$watch('showPublic', $scope.load);
   });
