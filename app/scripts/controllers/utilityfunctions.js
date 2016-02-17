083d052599c7c8b53c0fd1353b98e7810d2f2d12
havahol <havard.heitlo.holm@sintef.no>
2016-02-17 11:19:51 +0100

    Successfully duplicated view - content missing

--- app/scripts/controllers/utilityfunctions.js
+++ app/scripts/controllers/utilityfunctions.js
@@ -8,14 +8,27 @@
  * Controller of the grafterizerApp
  */
 angular.module('grafterizerApp')
-  .controller('UtilityFunctionsCtrl', function ($scope, dataGraftApi) {
+  .controller('UtilityFunctionsCtrl', function (
+    $scope,
+    $mdDialog,
+    dataGraftApi) {
+    
+    // Some auto generated boiler code:
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     
-    var utilityFunctionsObj = [];
+    
+    $scope.consoleTest = function() {
+        console.log("This is the consoleTest functions");
+    }
+    $scope.log = function(a) {
+        console.log(a);
+    }
+    
+    $scope.utilityFunctionsObj = [];
     $scope.utilityFunctions = "";
     $scope.showPublicUtilityFunctions = true;
     
@@ -32,7 +45,7 @@ angular.module('grafterizerApp')
                 if ($scope.showPublicUtilityFunctions) {
                     pretifyUtilityFunctions(data);
                 } else {
-                    utilityFunctionsObj = [];
+                    $scope.utilityFunctionsObj = [];
                     
                     for (var i in data) {
                         dataGraftApi.utilityFunctionGet(data[i]["id"]).success( function(singleData) {
@@ -42,21 +55,51 @@ angular.module('grafterizerApp')
                                 for (var j in data) {
                                     if (singleData["id"] === data[j]["id"]) {
                                         console.log("pushing data[" + j + "]");
-                                        utilityFunctionsObj.push(data[j]);
+                                        $scope.utilityFunctionsObj.push(data[j]);
                                         break;
                                     }
                                 }
-                                pretifyUtilityFunctions(utilityFunctionsObj);
+                                pretifyUtilityFunctions($scope.utilityFunctionsObj);
                             }
                         });
                     }
-                    console.log(utilityFunctionsObj);
-                    pretifyUtilityFunctions(utilityFunctionsObj);
+                    console.log($scope.utilityFunctionsObj);
+                    pretifyUtilityFunctions($scope.utilityFunctionsObj);
                 }
             }
         );
     }
     $scope.listUtilityFunctions();
+    
+    $scope.selectedUtilityFunction = {};
+    $scope.setSelectedUtilityFunction = function(uf) {
+        console.log("Selecting utility function: " + uf);
+        $scope.selectedUtilityFunction = uf;
+    }
+    
+    $scope.deleteUtilityFunction = function(id) { 
+        console.log("Not implemented: Delete utility function with id " + id);
+    }
+    
+    $scope.createNewUtilityFunction = function() {
+        console.log("Not implemented: Create new utility function");
+    }
+    
+    $scope.createNewTextTransformation = function() {
+        console.log("Not implemneted: Create new text transformation");
+    }
+    
+    $scope.applyUtilityFunctionChanges = function() {
+        console.log("Not implemented: apply utility function changes");
+    }
+    
+    $scope.cancelUtilityFunctionChanges = function() {
+        console.log("Not implemented: cancel utility function changes");
+    }
+    
+    $scope.showClojureCode = function(uf) {
+        console.log("Not implemented: show clojure code for " + uf);
+    }
     
     
     var pretifyUtilityFunctions = function(ufList) {
@@ -68,6 +111,26 @@ angular.module('grafterizerApp')
     }
     
     
+    // This function should open the dialog box and set controller for it.
+    // Let's try to use the same controller as we're already in :)
+    $scope.defineCustomFunctions = function() {
+      //$scope.originalCustomFunctionDeclarations = [];
+      //angular.copy($scope.transformation.customFunctionDeclarations, $scope
+      //    .originalCustomFunctionDeclarations);
+      $mdDialog.show({
+        templateUrl: 'views/havaholDialog.html',
+        controller: 'UtilityFunctionsCtrl',
+        scope: $scope.$new(false, $scope),
+        clickOutsideToClose: true
+      }).then(
+        function() {});//,
+
+        //function() {
+        //  angular.copy($scope.originalCustomFunctionDeclarations, $scope.transformation
+        //    .customFunctionDeclarations);
+        //});
+    };
+    
     
     $scope.apiKey = dataGraftApi.apiKey;
 
@@ -76,6 +139,8 @@ angular.module('grafterizerApp')
     var metadata = ['{"weather": "sunny", "smurning": "vr40", "good": "oyeah"}',
                     '{"metadata": "yeah"}',
                     '{"metadata": "yeah", "snow": "some"}'];
+    
+    $scope.utilityFunctionsObj = metadata;
     var id = "1";
     $scope.testText = "Currently nothing is tested";
     
